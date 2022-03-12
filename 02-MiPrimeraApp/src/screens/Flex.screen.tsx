68f8aed2aea870ef1d73b4ecb180081146a2d2db
victor19pa <vpinedaa@unah.hn>
2022-03-12 16:59:20 -0600

    flex wrap

--- 02-MiPrimeraApp/src/screens/Flex.screen.tsx
+++ 02-MiPrimeraApp/src/screens/Flex.screen.tsx
@@ -7,6 +7,34 @@ const Flex = () => {
       <Text style={styles.caja1}>Caja 1</Text>
       <Text style={styles.caja2}>Caja 3</Text>
       <Text style={styles.caja3}>Caja 2</Text>
+
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
+
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
+      
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
+      
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
+
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
+
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
+
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 3</Text>
+      <Text style={styles.caja3}>Caja 2</Text>
     </View>
   )
 }
@@ -15,30 +43,28 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     backgroundColor: '#28C4D9',
-    // justifyContent: 'flex-end',
-    // alignItems: 'flex-end',
-    // flexDirection: 'row'
-    
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+
+    flexWrap: 'wrap',
   },
   caja1: {
     borderWidth:2,
     borderColor: 'white',
     fontSize: 30,
     flex: 0,
-    alignSelf: 'center'
   },
   caja2:{
     borderWidth:2,
     borderColor: 'white',
     fontSize: 30,
-    alignSelf: 'flex-start'
     
   },
   caja3:{
     borderWidth:2,
     borderColor: 'white',
     fontSize: 30,
-    alignSelf: 'flex-end'
   }
 });
 
