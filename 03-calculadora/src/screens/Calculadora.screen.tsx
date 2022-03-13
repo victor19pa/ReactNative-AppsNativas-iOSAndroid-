3b902527dd2bb7f35b86e80eb86f7d3f59b5bc7c
victor19pa <vpinedaa@unah.hn>
2022-03-12 23:05:02 -0600

    feat: textos y primer boton

--- 03-calculadora/src/screens/Calculadora.screen.tsx
+++ 03-calculadora/src/screens/Calculadora.screen.tsx
@@ -4,8 +4,16 @@ import { styles } from '../theme/appTheme';
 
 const Calculadora = () => {
   return (
-    <View>
-      <Text style={styles.texto}>Calculadora</Text>
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+
+      <View>
+        {/*Boton */}
+        <View style={styles.boton}>
+          <Text style={styles.botonTxt}>1</Text>
+        </View>
+      </View>
     </View>
   )
 }
