9df448583328092ca38c5631ece1bc010513f3ad
victor19pa <vpinedaa@unah.hn>
2022-03-13 15:24:26 -0600

    feat: consideraciones numeros

--- 03-calculadora/src/screens/Calculadora.screen.tsx
+++ 03-calculadora/src/screens/Calculadora.screen.tsx
@@ -13,15 +13,45 @@ const Calculadora = () => {
   }
 
   const armarNumero = (numeroTxt: string) => {
-    setNumero(numero + numeroTxt)
+    //no doble punto
+    if (numero.includes('.') && numeroTxt === '.') return;
+
+    if (numero.startsWith('0') || numero.startsWith('-0')) {
+      //punto decimal
+      if (numeroTxt === '.') {
+        setNumero(numero + numeroTxt);
+      } else if (numeroTxt === '0' && numero.includes('.')) {
+        //evaluar si es otro cero y hay un punto
+        setNumero(numero + numeroTxt);
+      } else if (numeroTxt !== '0' && !numero.includes('.')) {
+        //evaluar si es diferente de 0 y no tiene punto
+        setNumero(numeroTxt);
+      } else if( numeroTxt === '0' && !numero.includes('.')){
+        //evitar el 000.0
+        setNumero( numero )
+      }else{
+        setNumero( numero + numeroTxt)
+      }
+
+    } else {
+      setNumero(numero + numeroTxt)
+    }
+  }
+
+  const positivoNegativo = () => {
+    if (numero.includes('-')) {
+      setNumero(numero.replace('-', ''));
+    } else {
+      setNumero('-' + numero);
+    }
   }
 
   return (
     <View style={styles.calculadoraContainer}>
       <Text style={styles.resultadoPequeno}>{numeroAnterior}</Text>
-      <Text 
+      <Text
         style={styles.resultado}
-        numberOfLines={ 1 }
+        numberOfLines={1}
         adjustsFontSizeToFit //true
       >
         {numero}
@@ -29,7 +59,7 @@ const Calculadora = () => {
 
       <View style={styles.fila}>
         <BotonCalculadora texto='C' color='#9B9B9B' handleClick={limpiar} />
-        <BotonCalculadora texto='+/-' color='#9B9B9B' handleClick={limpiar} />
+        <BotonCalculadora texto='+/-' color='#9B9B9B' handleClick={positivoNegativo} />
         <BotonCalculadora texto='del' color='#9B9B9B' handleClick={limpiar} />
         <BotonCalculadora texto='/' color='#FF9427' handleClick={limpiar} />
       </View>
