d1129555b4170ad8dc9767fcb354022cdab500d1
victor19pa <vpinedaa@unah.hn>
2022-03-14 22:43:34 -0600

    :heavy_plus_sign: add: react navigation @6 install

--- 04-navegacionApp/android/app/src/main/java/com/navegacionapp/MainActivity.java
+++ 04-navegacionApp/android/app/src/main/java/com/navegacionapp/MainActivity.java
@@ -1,5 +1,5 @@
 package com.navegacionapp;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +11,9 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "navegacionApp";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
