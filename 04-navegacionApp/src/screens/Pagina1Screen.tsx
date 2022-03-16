9356dae4bd7b830a6196678da6cd6d3edee53e38
victor19pa <vpinedaa@unah.hn>
2022-03-15 22:52:09 -0600

    :sparkles: feat: toggle drawer

--- 04-navegacionApp/src/screens/Pagina1Screen.tsx
+++ 04-navegacionApp/src/screens/Pagina1Screen.tsx
@@ -1,12 +1,24 @@
-import { StackScreenProps } from '@react-navigation/stack';
-import React from 'react'
-import { Button, Text, View } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useEffect } from 'react'
+import { Button, Text, View, TouchableOpacity } from 'react-native'
+import { DrawerScreenProps } from '@react-navigation/drawer';
+// import { StackScreenProps } from '@react-navigation/stack';
 import styles from '../theme/appTheme';
 
-interface Props extends StackScreenProps<any, any> { };
+interface Props extends DrawerScreenProps<any, any> { };
 
 const Pagina1Screen = ({ navigation }: Props) => {
+
+  // useEffect(() => {
+  //   navigation.setOptions({
+  //     headerLeft: () => (
+  //       <Button 
+  //         title='Menu'
+  //         onPress={ () => navigation.toggleDrawer()}
+  //       />
+  //     )
+  //   })
+  // }, [])
+  
 
   return (
     <View style={styles.globalMargin}>
