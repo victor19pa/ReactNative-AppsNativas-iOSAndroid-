447256e2a08ef42264baa3474eac601e753dbc96
victor19pa <vpinedaa@unah.hn>
2022-03-20 21:28:33 -0600

    :sparkles: feat: personalizar btn Tab nav

--- 04-navegacionApp/src/navigator/Tabs.tsx
+++ 04-navegacionApp/src/navigator/Tabs.tsx
@@ -3,15 +3,29 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Tab1Screen from '../screens/Tab1Screen';
 import Tab2Screen from '../screens/Tab2Screen';
 import Tab3Screen from '../screens/Tab3Screen';
+import StackNavigator from './StackNavigator';
+import styles, { colores } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+    <Tab.Navigator
+    sceneContainerStyle={{
+      backgroundColor: 'white',
+    }}
+    screenOptions={{
+      tabBarActiveTintColor: colores.primary,
+      tabBarStyle: {
+        borderTopColor: colores.primary,
+        borderTopWidth: 0,
+        elevation: 0
+      },
+    }}
+    >
+      <Tab.Screen name="Tab1Screen" options={{title: 'Tab 1'}} component={Tab1Screen} />
+      <Tab.Screen name="Tab2Screen" options={{title: 'Tab 2'}} component={Tab2Screen} />
+      <Tab.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
     </Tab.Navigator>
   );
 }
--- 04-navegacionApp/src/screens/Tab1Screen.tsx
+++ 04-navegacionApp/src/screens/Tab1Screen.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { useEffect } from 'react';
 
 const Tab1Screen = () => {
+
+  useEffect(() => {
+    console.log(('Hola'));
+    
+  }, [])
+  
+
   return (
     <View>
       <Text>Tabs 1</Text>  
