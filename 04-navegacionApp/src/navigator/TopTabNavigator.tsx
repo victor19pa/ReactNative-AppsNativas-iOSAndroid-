3448c4b7c57f91dd37f050d3e8d4d5becbac57b3
victor19pa <vpinedaa@unah.hn>
2022-03-21 19:36:35 -0600

    :sparkles: feat: personalizat top tab

--- 04-navegacionApp/src/navigator/TopTabNavigator.tsx
+++ 04-navegacionApp/src/navigator/TopTabNavigator.tsx
@@ -3,12 +3,55 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import ChatScreen from '../screens/ChatScreen';
 import ContactsScreen from '../screens/ContactsScreen';
 import AlbumsScreen from '../screens/AlbumsScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colores } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+
+  const { top: paddingTop } = useSafeAreaInsets();
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{
+        paddingTop
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, focused, }) => {
+          let iconName: string = ''
+          switch (route.name) {
+            case 'ChatScreen':
+              iconName = 'CH'
+              break;
+
+            case 'ContactsScreen':
+              iconName = 'CS'
+              break;
+
+            case 'AlbumsScreen':
+              iconName = 'Al'
+              break;
+          }
+          return (
+            <Text style={{ color }}>{iconName}</Text>
+          )
+        },
+        tabBarPressColor: colores.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: colores.primary
+        },
+        tabBarStyle: {
+          borderTopColor: colores.primary,
+          borderTopWidth: 0,
+          elevation: 0
+        },
+      })}
+      sceneContainerStyle={{
+        backgroundColor: 'white'
+      }}
+    >
       <Tab.Screen name="ChatScreen" options={{ title: 'Chats'}}  component={ChatScreen} />
       <Tab.Screen name="ContactsScreen" options={{ title: 'Contacts'}} component={ContactsScreen} />
       <Tab.Screen name="AlbumsScreen" options={{ title: 'Albums'}} component={AlbumsScreen} />
