fdc3e5168658218c3f21e5af1f07c724f0402db1
victor19pa <vpinedaa@unah.hn>
2022-03-21 20:07:55 -0600

    :heavy_plus_sign: add: vector icons

--- 04-navegacionApp/src/screens/Tab1Screen.tsx
+++ 04-navegacionApp/src/screens/Tab1Screen.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { useEffect } from 'react';
+import styles from '../theme/appTheme';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const Tab1Screen = () => {
-
-  useEffect(() => {
-    console.log(('Hola'));
-    
-  }, [])
   
 
   return (
-    <View>
-      <Text>Tabs 1</Text>  
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Iconos</Text>  
+      {/* <Text>
+        <Icon name='airplane-outline' size={30} color='#900'/>
+      </Text> */}
+        <Icon name="game-controller-outline" size={30} color="#900" />
     </View>
   )
 }
