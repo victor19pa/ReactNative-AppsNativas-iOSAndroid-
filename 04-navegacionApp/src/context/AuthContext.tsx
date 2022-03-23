b3895666a0e7c9cae7cc4a0a392dfbcf1e684888
victor19pa <vpinedaa@unah.hn>
2022-03-22 22:56:46 -0600

    :sparkles: feat: change fav icon w context

--- 04-navegacionApp/src/context/AuthContext.tsx
+++ 04-navegacionApp/src/context/AuthContext.tsx
@@ -19,6 +19,7 @@ export const authInitialState: AuthState = {
 export interface AuthContextProps {
   authState: AuthState;
   signIn: () => void;
+  changeFavoriteIcon: (iconName: string) => void;
 }
 
 //crear context
@@ -32,11 +33,18 @@ export const AuthProvider = ({ children }: any) => {
   const signIn = () => {
     dispatch({ type: 'signIn'}) 
   }
+  const changeFavoriteIcon = ( iconName : string) => {
+    dispatch({ 
+      type: 'changeFavIcon', 
+      payload: iconName
+    })
+  }
   return (
     <AuthContext.Provider 
       value={{
         authState,
-        signIn
+        signIn,
+        changeFavoriteIcon
       }}
     >
       {children}
--- 04-navegacionApp/src/screens/SettingsScreen.tsx
+++ 04-navegacionApp/src/screens/SettingsScreen.tsx
@@ -1,26 +1,37 @@
 import React, { useContext } from 'react'
 import { View, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import styles from '../theme/appTheme';
+import styles, { colores } from '../theme/appTheme';
 import { AuthContext } from '../context/AuthContext';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const SettingsScreen = () => {
-  const {top} = useSafeAreaInsets();
+  const { top } = useSafeAreaInsets();
 
   const context = useContext(AuthContext);
   const { authState } = context;
 
   // console.log(authState);
-  
+
   return (
     <View style={{
       ...styles.globalMargin,
-      marginTop: top + 20 
-      }}
+      marginTop: top + 20
+    }}
     >
-      <Text style={{color: 'blue'}}>Settings Screen</Text>
+      <Text style={{ color: 'blue' }}>Settings Screen</Text>
 
-      <Text> { JSON.stringify(authState, null , 4 ) }</Text>
+      <Text> {JSON.stringify(authState, null, 4)}</Text>
+
+      {
+        authState.favoriteIcon && (
+          <Icon
+            name={authState.favoriteIcon}
+            size={200}
+            color={colores.primary}
+          />
+        )
+      }
     </View>
   )
 }
