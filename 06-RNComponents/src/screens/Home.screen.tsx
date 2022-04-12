820a9e0f14cca23dc44ea8e28a92fb39a5605ac0
victor19pa <vpinedaa@unah.hn>
2022-04-11 23:41:05 -0600

    :sparkles: feat: flatlist

--- 06-RNComponents/src/screens/Home.screen.tsx
+++ 06-RNComponents/src/screens/Home.screen.tsx
@@ -1,13 +1,59 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
+import { FlatList, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/appTheme';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  components: string;
+}
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animation101Screen'
+  },
+  {
+    name: 'Animation 102',
+    icon: 'star-outline',
+    components: 'Animation102Screen'
+  }
+]
 const Home = () => {
+
+  const { top } = useSafeAreaInsets();
+
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text style={{ color: 'black' }}>{menuItem.name} - {menuItem.icon}</Text>
+      </View>
+    )
+  }
+
+  const renderListHeader = () => {
+    return (
+      <View style={{ marginTop: top + 20, marginBottom: 20 }}>
+        <Text style={styles.title}>Opciones de menu</Text>
+      </View>
+    )
+  }
+
+  const itemSeparator = () => {
+    return (
+      <View style={{ borderBottomWidth: 1, opacity: 0.4, marginVertical: 8 }} />
+    )
+  }
+
   return (
-    <View>
-      <Icon
-        name='star-outline'
-        size={50}
-        color='black'
+    <View style={{ flex: 1, ...styles.globalMargin }}>
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={itemSeparator}
       />
     </View>
   )
