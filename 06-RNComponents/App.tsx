afd01a0f8e8534b3edc2b7278d306b5ea3d1428b
victor19pa <vpinedaa@unah.hn>
2022-04-11 23:14:39 -0600

    :wastebasket: delete tailwind

--- 06-RNComponents/App.tsx
+++ 06-RNComponents/App.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Stack } from './src/navigator';
 import { LogBox } from 'react-native';
-import utilities from './tailwind.json';
-import { TailwindProvider } from 'tailwind-rn/dist';
+
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
 ]);
@@ -12,11 +11,9 @@ LogBox.ignoreLogs([
 const App = () => {
 
   return (
-    <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <Stack />
-      </NavigationContainer>
-    </TailwindProvider>
+    <NavigationContainer>
+      <Stack />
+    </NavigationContainer>
   );
 };
 
--- 06-RNComponents/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{tsx,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
-};
