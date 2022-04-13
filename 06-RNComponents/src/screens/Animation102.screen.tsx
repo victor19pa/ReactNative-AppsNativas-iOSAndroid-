823831b04d445de7edd1200991425ddd40575171
victor19pa <vpinedaa@unah.hn>
2022-04-13 17:30:15 -0600

    :sparkles: feat: animated value XY

--- 06-RNComponents/src/screens/Animation102.screen.tsx
+++ 06-RNComponents/src/screens/Animation102.screen.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useRef } from 'react'
+import { Animated, PanResponder, StyleSheet, View } from 'react-native'
 
 const Animation102 = () => {
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      { dx: pan.x, dy: pan.y, },
+    ], {
+      useNativeDriver: false
+    }),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        { toValue: { x: 0, y: 0 }, useNativeDriver: false } // Back to zero
+      ).start();
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.purbleBox}>
-
-      </View>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.purbleBox]}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   purbleBox: {
-    backgroundColor: 'red',
+    backgroundColor: '#75cedb',
     width: 150,
     height: 150
   },
