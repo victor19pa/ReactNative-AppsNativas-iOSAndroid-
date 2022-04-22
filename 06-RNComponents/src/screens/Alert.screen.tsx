5820aa46ead928a82995d91f5b85979de2d43820
victor19pa <vpinedaa@unah.hn>
2022-04-21 20:20:25 -0600

    :sparkles: feat: promp android

--- 06-RNComponents/src/screens/Alert.screen.tsx
+++ 06-RNComponents/src/screens/Alert.screen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Alert, Button, View } from 'react-native'
+import prompt from 'react-native-prompt-android'
 import { HeaderTitle } from '../components/atoms'
 import { styles } from '../theme/appTheme'
 
@@ -27,6 +28,31 @@ const AlertScreen = () => {
     )
   }
 
+  const showPrompt = () => {
+    // solo ios
+    // Alert.prompt(
+    //   'Esta seguro?',
+    //   'Esta accion no se puede revertir',
+    //   (valor: string) => console.log('info ', valor),
+    //   'plain-text',
+    //   'Hola mundo'
+    // )
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder'
+      }
+    );
+  }
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title='Alerts' />
@@ -34,6 +60,13 @@ const AlertScreen = () => {
       <Button
         title='Mostrar Alerta'
         onPress={showAlert}
+      />
+
+      <View style={{ height: 10 }} />
+
+      <Button
+        title='Mostrar Alerta (solo iOS)'
+        onPress={showPrompt}
       />
     </View>
   )
