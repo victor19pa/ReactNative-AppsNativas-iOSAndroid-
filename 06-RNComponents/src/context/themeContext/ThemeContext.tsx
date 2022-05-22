4ff112d9b0993ec7c74845abcd0dc648e644fce8
victor19pa <vpinedaa@unah.hn>
2022-05-21 21:46:05 -0600

    :sparkles: feat: set theme with software theme

--- 06-RNComponents/src/context/themeContext/ThemeContext.tsx
+++ 06-RNComponents/src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from "react";
-import { lightTheme, themeReducer, ThemeState } from "./themeReducer";
+import React, { createContext, useEffect, useReducer } from "react";
+import { Appearance, AppState, useColorScheme } from "react-native";
+import { darkTheme, lightTheme, themeReducer, ThemeState } from "./themeReducer";
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -10,8 +11,29 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
+  const colorScheme = useColorScheme();
 
-  const [theme, dispatch] = useReducer(themeReducer, lightTheme) // TODO initial state
+  const [theme, dispatch] = useReducer(
+    themeReducer,
+    (Appearance.getColorScheme() === 'dark') ? darkTheme : lightTheme
+  );
+
+  useEffect(() => {
+    AppState.addEventListener('change', (status) => {
+      if (status === 'active') {
+        (Appearance.getColorScheme() === 'light')
+          ? setLightTheme
+          : setDarkTheme
+      }
+    })
+  }, [])
+
+  // ! solo ios
+  // useEffect(() => {
+  //   (colorScheme === 'light')
+  //     ? setLightTheme
+  //     : setDarkTheme
+  // }, [colorScheme])
 
   const setDarkTheme = () => {
     dispatch({
